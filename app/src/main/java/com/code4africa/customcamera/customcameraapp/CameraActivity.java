75db1a9eb5d510b2b2959c568caf87c7e65288c1
Seremba John <john.seremba@andela.com>
2017-09-11 15:13:41 +0300

    Open & close Camera Device

--- app/src/main/java/com/code4africa/customcamera/customcameraapp/CameraActivity.java
+++ app/src/main/java/com/code4africa/customcamera/customcameraapp/CameraActivity.java
@@ -3,23 +3,24 @@ package com.code4africa.customcamera.customcameraapp;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.SurfaceTexture;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.TextureView;
 import android.view.View;
-import android.widget.Button;
 import android.hardware.camera2.CameraDevice;
-import android.hardware.camera2.CameraManager;
-import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class CameraActivity extends AppCompatActivity {
 	private static final String TAG = CameraActivity.class.getSimpleName();
 	private TextureView textureView;
+	private CameraDevice cameraDevice;
 
 	private TextureView.SurfaceTextureListener surfaceTextureListener = new TextureView.SurfaceTextureListener() {
 		@Override public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
-
+			Toast.makeText(getApplicationContext(), "Surface texture is available", Toast.LENGTH_SHORT).show();
 		}
 
 		@Override
@@ -35,6 +36,35 @@ public class CameraActivity extends AppCompatActivity {
 
 		}
 	};
+
+	private CameraDevice.StateCallback cameraDeviceStateCallback = new CameraDevice.StateCallback() {
+		@Override public void onOpened(@NonNull CameraDevice camera) {
+			cameraDevice = camera;
+		}
+
+		@Override public void onDisconnected(@NonNull CameraDevice camera) {
+			camera.close();
+			cameraDevice = null;
+		}
+
+		@Override public void onError(@NonNull CameraDevice camera, int i) {
+			camera.close();
+			cameraDevice = null;
+			Log.w(TAG, "Error opening camera: ");
+		}
+	};
+
+	private void closeCamera() {
+		if(cameraDevice != null) {
+			cameraDevice.close();
+			cameraDevice = null;
+		}
+	}
+
+	@Override protected void onPause() {
+		closeCamera();
+		super.onPause();
+	}
 
 	@Override
 	public void onWindowFocusChanged(boolean hasFocus) {
@@ -55,6 +85,8 @@ public class CameraActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_camera);
 
+		textureView = (TextureView) findViewById(R.id.tv_camera);
+
 	}
 
 	@Override
@@ -64,6 +96,12 @@ public class CameraActivity extends AppCompatActivity {
 
 	@Override protected void onResume() {
 		super.onResume();
+
+		if(textureView.isAvailable()) {
+
+		} else {
+			textureView.setSurfaceTextureListener(surfaceTextureListener);
+		}
 	}
 
 	private boolean checkCameraHardware(Context context) {
