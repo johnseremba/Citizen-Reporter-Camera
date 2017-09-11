e7880cd639804eb816928eb93d71c9b0c98e992b
Seremba John <john.seremba@andela.com>
2017-09-11 14:49:10 +0300

    Add camera icons. .Create Texture View. .intialize SurfaceTextureListener

--- app/src/main/java/com/code4africa/customcamera/customcameraapp/CameraActivity.java
+++ app/src/main/java/com/code4africa/customcamera/customcameraapp/CameraActivity.java
@@ -1,25 +1,60 @@
 package com.code4africa.customcamera.customcameraapp;
 
-import android.annotation.SuppressLint;
-import android.support.v7.app.ActionBar;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
+import android.util.Log;
+import android.view.TextureView;
 import android.view.View;
-
+import android.widget.Button;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.widget.TextView;
 
 public class CameraActivity extends AppCompatActivity {
-	private View mContentView;
-	private View mControlsView;
+	private static final String TAG = CameraActivity.class.getSimpleName();
+	private TextureView textureView;
+
+	private TextureView.SurfaceTextureListener surfaceTextureListener = new TextureView.SurfaceTextureListener() {
+		@Override public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
+
+		}
+
+		@Override
+		public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) {
+
+		}
+
+		@Override public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+			return false;
+		}
+
+		@Override public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+
+		}
+	};
+
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+		super.onWindowFocusChanged(hasFocus);
+		View decorView = getWindow().getDecorView();
+		if(hasFocus){
+			decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+			| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+			| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+			| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+			| View.SYSTEM_UI_FLAG_FULLSCREEN
+			| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+		}
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_camera);
 
-		mControlsView = findViewById(R.id.fullscreen_content_controls);
-		mContentView = findViewById(R.id.fullscreen_content);
 	}
 
 	@Override
@@ -27,5 +62,21 @@ public class CameraActivity extends AppCompatActivity {
 		super.onPostCreate(savedInstanceState);
 	}
 
+	@Override protected void onResume() {
+		super.onResume();
+	}
 
+	private boolean checkCameraHardware(Context context) {
+		return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
+	}
+
+	public static CameraDevice getCameraInstance() {
+		CameraDevice camera = null;
+		try {
+			//camera = CameraDevice;
+		} catch (Exception e) {
+			Log.d(TAG, "Camera not available: " + e);
+		}
+		return camera;
+	}
 }
