4f7978c2ad9720e6d1648bbee254acc0a6dbf8df
Seremba John <john.seremba@andela.com>
2017-09-15 18:04:38 +0300

    Create image view activity

---
+++ app/src/main/java/com/code4africa/customcamera/customcameraapp/ViewImageActivity.java
@@ -0,0 +1,21 @@
+package com.code4africa.customcamera.customcameraapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class ViewImageActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_view_image);
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+	}
+}
