f802ca7f28865b1f662e13feabe450e8f812a204
Seremba John <john.seremba@andela.com>
2017-10-09 16:17:20 +0300

    Test for camera initial state

--- app/src/androidTest/java/com/code4africa/customcamera/customcameraapp/CameraActivityInitialState.java
+++ app/src/androidTest/java/com/code4africa/customcamera/customcameraapp/CameraActivityInitialState.java
@@ -1,8 +1,10 @@
 package com.code4africa.customcamera.customcameraapp;
 
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+import java.util.ArrayList;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +13,7 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.Matchers.not;
 
@@ -45,40 +48,32 @@ public class CameraActivityInitialState {
 
 	@Test
 	public void absenceOfOtherIcons() {
-		onView(withId(R.id.img_capture))
-				.check(matches(not(isDisplayed())));
+		ArrayList<Integer> goneList = new ArrayList<Integer>() {
+			{
+				add(R.id.txt_swipe_caption);
+				add(R.id.sw_swipe_1);
+				add(R.id.sw_swipe_2);
+				add(R.id.sw_swipe_3);
+				add(R.id.sw_swipe_4);
+				add(R.id.sw_swipe_5);
 
-		onView(withId(R.id.img_flash_btn))
-				.check(matches(not(isDisplayed())));
+				add(R.id.img_capture);
+				add(R.id.img_flash_btn);
+				add(R.id.img_switch_camera);
+				add(R.id.img_gallery);
+				add(R.id.img_effects_btn);
+				add(R.id.img_wb_btn);
+				add(R.id.seekbar_light);
+			}
+		};
+		testIsGone(goneList);
+	}
 
-		onView(withId(R.id.img_switch_camera))
-				.check(matches(not(isDisplayed())));
-
-		onView(withId(R.id.img_gallery))
-				.check(matches(not(isDisplayed())));
-
-		onView(withId(R.id.img_effects_btn))
-				.check(matches(not(isDisplayed())));
-
-		onView(withId(R.id.img_wb_btn))
-				.check(matches(not(isDisplayed())));
-
-		onView(withId(R.id.seekbar_light))
-				.check(matches(not(isDisplayed())));
-
-		onView(withId(R.id.sw_swipe_1))
-				.check(matches(not(isDisplayed())));
-		onView(withId(R.id.sw_swipe_2))
-				.check(matches(not(isDisplayed())));
-		onView(withId(R.id.sw_swipe_3))
-				.check(matches(not(isDisplayed())));
-		onView(withId(R.id.sw_swipe_4))
-				.check(matches(not(isDisplayed())));
-		onView(withId(R.id.sw_swipe_5))
-				.check(matches(not(isDisplayed())));
-
-		onView(withId(R.id.txt_swipe_caption))
-				.check(matches(not(isDisplayed())));
+	public void testIsGone(ArrayList<Integer> list) {
+		for (Integer id : list) {
+			onView(withId(id))
+					.check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+		}
 	}
 
 }
